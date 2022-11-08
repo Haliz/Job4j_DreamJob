a64ef72c37d91d7a58bd8ddcedb2c9aa6caf047f
Haliz <foril2n1@rambler.ru>
2022-11-08 09:02:44 +0300

    4. Thymeleaf, Циклы.(Исправление2)

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,9 +14,9 @@ public class PostStore {
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "Description for Junior", "01/10/2022"));
-        posts.put(2, new Post(2, "Middle Java Job", "Description for Middle", "02/10/2022"));
-        posts.put(3, new Post(3, "Senior Java Job", "Description for Senior", "03/10/2022"));
+        posts.put(1, new Post(1, "Junior Java Job", "Description for Junior", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "Description for Middle", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "Description for Senior", LocalDateTime.now()));
     }
 
     public static PostStore instOf() {
